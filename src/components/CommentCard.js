6a10f7715d18352ee663877aefaa9b7943511a08
fedog-ops <felixtmcd@outlook.com>
2023-02-06 16:05:52 +0000

    delete comments

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,31 +1,49 @@
 import React from 'react'
-import { Comment } from 'semantic-ui-react'
-import { Avatar } from '@mui/material'
+
+import { Avatar, Typography } from '@mui/material'
 import { red } from "@mui/material/colors";
-
-const CommentCard = ({data}) => {
-  console.log('commentsCard component')
-    return (
-  <Comment.Group>
+import {Box, Stack, IconButton} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from 'react';
+const CommentCard = ({data, canDelete, handleDeleteComment}) => {
+  const [commentDeleted, setCommentsDeleted] = useState(false)
+  const handleDeleteClick = () => {
+    handleDeleteComment(data.comment_id)
+    setCommentsDeleted(true)
+  }
+if (commentDeleted) return (<Box sx={{width: 3/4}}><Typography>Deleted ... </Typography></Box>)
+return (
+  <Box>
+    <Stack direction="row"
+        justifyContent="space-around"
+        
+        sx={{
+          gap: { sm: "25px", xs: "8px" },
+          mt: { sm: "25px", xs: "4px" },
+          justifyContent: "none",
+        }}
+        px="20px">
    
-    <Comment>
-    <Avatar sx={{ bgcolor: red[500], height:25, width: 25 }} aria-label="review">
-            {data.author.slice(0, 2)}
-          </Avatar>
-        <Comment.Content> 
-        <Comment.Author as='a'>{data.author}</Comment.Author>
-        <Comment.Metadata>
-          <div>{data.created_at}</div>
-        </Comment.Metadata>
-        <Comment.Text>{data.body}</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
-
-   
-  </Comment.Group>
+    <Box>
+      <Avatar sx={{ bgcolor: red[500], height:50, width: 50 }} aria-label="review">
+              {data.author.slice(0, 2)}
+      </Avatar>
+    </Box>
+      <Box sx={{ width: 3/4 }}>
+        <Stack direction="row" sx={{gap: "8px"}} >
+        <Typography>{data.author}</Typography>
+       
+          <Typography>{data.created_at.slice(0, 10)}</Typography>
+      </Stack>
+        <Typography>{data.body}</Typography>
+        
+      
+      </Box>
+     {canDelete? (<IconButton onClick={handleDeleteClick}>
+        <DeleteIcon/>
+      </IconButton>) : null} 
+   </Stack>
+  </Box>
 )
 }
 
