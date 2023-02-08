0ae481fbd9ca40bd4a817fca51fb5e073b32b17b
fedog-ops <felixtmcd@outlook.com>
2023-02-08 18:09:32 +0000

    remove unused component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { useState } from 'react';
